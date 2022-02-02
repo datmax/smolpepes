50ac47014f3437756615808ed1f9f313ed11777f
Max Datsun <max.datsun@student.univaq.it>
2022-02-03 00:14:46 +0100

    letsgo

---
+++ layouts/default.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-app dark>
+    <v-main>
+      <v-container fluid>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
+      <v-list>
+        <v-list-item @click.native="right = !right">
+          <v-list-item-action>
+            <v-icon light> mdi-repeat </v-icon>
+          </v-list-item-action>
+          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-footer absolute app color="transparent">
+      <v-row align="center" justify="center" class="py-4">
+        <span>
+          <a href="https://twitter.com/smolpepes">
+            <v-icon>mdi-twitter</v-icon>
+          </a>
+        </span>
+      </v-row>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: "mdi-apps",
+          title: "Welcome",
+          to: "/",
+        },
+        {
+          icon: "mdi-chart-bubble",
+          title: "Inspire",
+          to: "/inspire",
+        },
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: "Vuetify.js",
+    };
+  },
+};
+</script>
+<style>
+* {
+  font-family: "Press Start 2P", cursive;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-row justify="center" align="center" class="a">
+    <v-col cols="12" sm="8" md="6" class="culo text-center">Hello. </v-col>
+  </v-row>
+</template>
+
+<style>
+.culo {
+  font-size: 8em;
+  padding-top: 30vh;
+}
+
+.a {
+  height: 100%;
+}
+</style>
